52c93da6f4f9bc90eb9dbc18e7fc1f402d604cb5
zhaojiachen <1069802938@qq.com>
2018-06-22 10:23:20 +0800

    go版本的Range

--- oop/func_demo2.go
+++ oop/func_demo2.go
@@ -1,10 +1,20 @@
 package main 
 import  std "fmt"
 
-func Range(start int ,stop int ,step int) [] int {
+func Range(start int ,end int ,step int) [] int {
+	var list [] int 
+	if start > end{
+		return list 
+	}
+	for t := start; t < end; t+=step {
+		if t < end{
+			list = append(list,t )
+		} 
+	}
 
+	return list 
 }
 
 func main(){
-	std.Println()
+	std.Println(Range(0,10,2))
 }
